b78727815b4c852bb04b545cb38305012420a2e6
Charles-François St-Cyr <charlesfrancois.stcyr@moov.ai>
2025-05-06 16:59:27 -0400

    Add Environment enum and environment handling to settings

--- src/config/settings/settings.py
+++ src/config/settings/settings.py
@@ -1,8 +1,18 @@
+from enum import Enum
 from pathlib import Path
 
 from pydantic import AliasChoices, Field, FilePath
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
+class Environment(Enum):
+    """
+    Enum for different environments.
+    """
+    DEVELOPMENT = "development"
+    PRODUCTION = "production"
+    TESTING = "testing"
+    STAGING = "staging"
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -19,3 +29,21 @@ class Settings(BaseSettings):
             "The file should follow the [logging configuration dictionary schema](https://docs.python.org/3/library/logging.config.html#logging-config-dictschema)"
         ),
     )
+
+    env: Environment = Field(
+        default=Environment.DEVELOPMENT,
+        validation_alias=AliasChoices("environment", "env"),
+        description="The environment in which the application is running.",
+    )
+
+    def is_dev(self) -> bool:
+        """
+        Check if the current environment is development.
+        """
+        return self.env == Environment.DEVELOPMENT
+    
+    def is_prod(self) -> bool:
+        """
+        Check if the current environment is production.
+        """
+        return self.env == Environment.PRODUCTION
