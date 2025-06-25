93c19dd3d747df3a63733ec85e36d504f7de2f8b
Charles-François St-Cyr <charlesfrancois.stcyr@moov.ai>
2025-06-25 13:20:16 -0400

    refactor: integrate Settings class into CliApp

--- src/app/main.py
+++ src/app/main.py
@@ -1,15 +1,22 @@
 import logging
 
-from app.settings import Settings
+from pydantic_settings import CliApp
+
 from lib_core.foo import bar
 from lib_core.logs import init_logs
+from lib_core.settings.app_base_settings import AppBaseSettings
 
 logger = logging.getLogger(__name__)
 
 
-def main(*, settings: Settings = Settings.create()):
-    init_logs(settings.logs)
+class Settings(AppBaseSettings):
+    def cli_cmd(self) -> None:
+        init_logs(self.logs)
 
-    logger.info("Starting application...")
+        logger.info("Starting application...")
 
-    bar()
+        bar()
+
+
+def main() -> None:
+    CliApp.run(Settings)
--- src/app/settings.py
+++
@@ -1,5 +0,0 @@
-from lib_core.settings import AppBaseSettings
-
-
-class Settings(AppBaseSettings):
-    pass
